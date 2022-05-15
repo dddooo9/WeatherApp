b33e84a8a1bd02dd1c706ec7b61a5d3b790cf9e2
dddooo9 <dlaehdus99@naver.com>
2022-05-16 02:33:31 +0900

    [FEAT] Activity 구현
    - adapter 연결
    - viewModel 연결

--- app/src/main/java/com/doyeon/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/doyeon/weatherapp/ui/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.doyeon.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.doyeon.weatherapp.R
+import com.doyeon.weatherapp.databinding.ActivityMainBinding
+import com.doyeon.weatherapp.domain.model.LocalWeather
+import com.doyeon.weatherapp.ui.adapter.WeatherAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var weatherAdapter: WeatherAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        setWeatherAdapter()
+        setObserver()
+
+        viewModel.searchWeather()
+    }
+
+    private fun setObserver() {
+        viewModel.localWeather.observe(this) {
+            val dataForAddHeader: MutableList<LocalWeather?> = it.toMutableList()
+            dataForAddHeader.add(0, null)
+
+            weatherAdapter.submitList(dataForAddHeader)
+        }
+    }
+
+    private fun setWeatherAdapter() {
+        weatherAdapter = WeatherAdapter()
+        binding.rvMainWeather.adapter = weatherAdapter
     }
 }
